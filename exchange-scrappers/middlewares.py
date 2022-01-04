1be4969eacf78903283421ff400a04acdc1b3bc9
joachim <joa_rodrigues@hotmail.com>
2022-01-04 22:56:59 +0100

    Added binance pairs alert

--- exchange-scrappers/middlewares.py
+++ exchange-scrappers/middlewares.py
@@ -2,6 +2,7 @@ import json
 from scrapy import signals
 from scrapy.http import TextResponse
 from exchange.CoinbaseApi import CoinbaseApi
+from exchange.BinanceApi import BinancePublicAPI
 
 
 class CoinbaseMiddleware:
@@ -15,6 +16,36 @@ class CoinbaseMiddleware:
     def process_request(self, request, spider):
         api = CoinbaseApi()
         crypto_currencies = api.get_currencies()
+
+        return TextResponse(
+            url=request.url,
+            status=200,
+            body=json.dumps(crypto_currencies).encode('UTF-8'),
+            request=request
+        )
+
+    def process_response(self, request, response, spider):
+        return response
+
+    def process_exception(self, request, exception, spider):
+        pass
+
+    def spider_opened(self, spider):
+        spider.logger.info('Spider opened: %s' % spider.name)
+
+
+
+class BinanceMiddleware:
+
+    @classmethod
+    def from_crawler(cls, crawler):
+        s = cls()
+        crawler.signals.connect(s.spider_opened, signal=signals.spider_opened)
+        return s
+
+    def process_request(self, request, spider):
+        api = BinancePublicAPI()
+        crypto_currencies = api.get_exchangeInfo()
 
         return TextResponse(
             url=request.url,
